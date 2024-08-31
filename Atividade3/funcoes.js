bb27d7d9be46f4f396fbd218e76a942636931315
EdyFS <edy.ferreira.santana@gmail.com>
2024-08-31 10:49:47 -0300

    Melhorar função exibir

--- Atividade3/funcoes.js
+++ Atividade3/funcoes.js
@@ -5,30 +5,45 @@ let valor;
 function armazenarValores(){
     //valoresDoUsuario = document.getElementById('lista').value;
     valor = document.getElementById('lista').value;
-    valoresDoUsuario.push(valor);
-    console.log(valoresDoUsuario);
-    console.log(valor);
+    valoresDoUsuario.push(valor); //adicionar os valores à lista
+    valoresDoUsuario.sort(); //ordenar os valores
+    //console.log(valoresDoUsuario);
+    //console.log(valor);
+    return valoresDoUsuario;
 }
 
 //apagar espaço
 function apagarCampo(){
-    valor = document.getElementById('lista').value;
-    valor = document.value('');
+    /*valor = document.getElementById('lista').value;
+    valor.value = '';*/
+    document.getElementById('lista').value = '';
 }
 
-//ordenar
+/*ordenar
 function ordenarValores(){
     valoresDoUsuario.sort();
+    return valoresDoUsuario;
 }
+    */
+
 //exibir array
 function exibirValores(){
-    document.write(valoresDoUsuario);
+    let listaPronta = document.getElementById('listaPronta');
+            listaPronta.innerHTML = ''; // limpar a lista antes de exibir os novos valores
+
+            valoresDoUsuario.forEach(function(valor){
+                let li = document.createElement('li');
+                li.textContent = valor;
+                listaPronta.appendChild(li);
+            });
 }
 
-exibirValores()
 //executar
-function executar(){
+function executar(event){
+    event.preventDefault();
     armazenarValores();
-    ordenarValores();
     apagarCampo();
+    exibirValores();
 }
+
+
