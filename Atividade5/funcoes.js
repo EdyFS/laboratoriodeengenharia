6d5572ae54c395f1db6b46b075b08758f6de477a
EdyFS <edy.ferreira.santana@gmail.com>
2024-09-24 21:51:07 -0300

    Criar atividade

---
+++ Atividade5/funcoes.js
@@ -0,0 +1,30 @@
+class Tarefa{
+    constructor (nome, descricao, status){
+
+    }
+    concluir(){
+
+    }
+    detalhar(){
+
+    }
+}
+
+class GerenciadorDeTarefas{
+    constructor ()
+    adicionarTarefa(Tarefa){
+
+    }
+    listarTarefas(){
+
+    }
+    marcarComoConcluida(index){
+
+    }
+    removerTarefa(index){
+
+    }
+    visualizarDetalhes(index){
+        
+    }
+}
